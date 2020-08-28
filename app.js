f700c7652f9b8d0707bc613f009abab7d15c9c13
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-08-28 13:18:16 -0400

    added error handling

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(routes);
 
+app.use((req, res, next) => {
+    const err = new Error("The requested page couldn't be found.");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ) {
+        // log err to database
+        // don't worry about this for now
+    } else {
+        console.error(err);
+    }
+    next(err);
+});
+
 module.exports = app;
