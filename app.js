9326ea09d19588cef7d05719c989f142f5595c74
Ammar <ar.nassri@gmail.com>
2020-08-28 12:43:31 -0400

    Phase 1 is done

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(routes);
+
+module.exports = app;
